9c1e097c2d460d76de435f11e8514a10eb389ad6
Mutairu Wasiu Opeyemi <147235314+Wasberry-spaceapps@users.noreply.github.com>
2026-06-22 11:19:16 +0100

    Change base public path from './' to '/'

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default {
     root: 'sources/', // Sources files (typically where index.html is)
     envDir: '../',  // Directory where the env file is located
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    base: './', // Public path (what's after the domain)
+    base: '/', // Public path (what's after the domain)
     server:
     {
         host: '0.0.0.0', // Listen on all interfaces for Replit
